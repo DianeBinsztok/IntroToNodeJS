b0e0878259d71365b905ccacc173f5b1c87237fa
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-21 10:36:59 +0200

    Nodemon installed

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const server = express();
 server.listen(process.env.PORT || 3000);
 
 server.get("/", (req, res) => {
-  res.send("Ceci est un test");
+  res.send("Ceci est un test - Nodemon tourne ...");
 });
